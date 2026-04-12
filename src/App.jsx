39db9018baa6c57fb9bf5d082d97be2a731c9f88
yasin shaikh! <ns321004@gmail.com>
2026-04-12 12:20:42 +0530

    broswer router enable

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import "./index.css";
 import Mouse from "./Mouse";
 import Pagenotfound from "./Page404";
 import Products from "./Product";
-import { HashRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 function App() {
   return (
     <>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Mouse />} />
           <Route path="/products" element={<Products />} />
           <Route path="/cart" element={<Cartlist />} />
           <Route path="*" element={<Pagenotfound />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </>
   );
 }
